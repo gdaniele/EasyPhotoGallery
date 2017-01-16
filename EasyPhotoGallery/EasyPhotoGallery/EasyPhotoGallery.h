af52a535aad158a23be677c0a23936d1e6b4490d
Giancarlo Daniele <gdaniele@stanford.edu>
2017-01-16 13:59:28 -0800

    Hello, world!
    * Creates empty framework

---
+++ EasyPhotoGallery/EasyPhotoGallery/EasyPhotoGallery.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyPhotoGallery.
+FOUNDATION_EXPORT double EasyPhotoGalleryVersionNumber;
+
+//! Project version string for EasyPhotoGallery.
+FOUNDATION_EXPORT const unsigned char EasyPhotoGalleryVersionString[];
---
+++ EasyPhotoGallery/EasyPhotoGallery/TestViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+@objc class TestViewController: UIViewController {
+
+  public func hi() {
+    print("hi")
+  }
+}
