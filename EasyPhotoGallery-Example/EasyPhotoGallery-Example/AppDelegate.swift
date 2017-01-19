fb9152555076f3b7269857c3d23fd96ba67e6ebd
Giancarlo Daniele <gdaniele@stanford.edu>
2017-01-18 19:52:42 -0800

    Fixes an issue where RxSwift was not found

--- EasyPhotoGallery-Example/EasyPhotoGallery-Example/AppDelegate.swift
+++ EasyPhotoGallery-Example/EasyPhotoGallery-Example/AppDelegate.swift
@@ -9,20 +9,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
     let layout = GalleryViewLayout(cellsAcross: 3, interitemSpacing: 4, lineSpacing: 6)
     let galleryVC = GalleryRootContainerViewController(layout: layout, navigationBarTitle: "Choose a picture")
-    galleryVC.delegate = self
     window?.rootViewController = galleryVC
     window?.makeKeyAndVisible()
 
     return true
-  }
-}
-
-extension AppDelegate: GalleryRootContainerViewControllerDelegate {
-  func didPickImage(galleryRootContainerViewController: GalleryRootContainerViewController, image: UIImage) {
-    print("pick it dude")
-  }
-
-  func didDismiss(galleryRootContainerViewController: GalleryRootContainerViewController) {
-    print("Dismiss")
   }
 }
